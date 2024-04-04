2478f613ed92c6460d3588d1ff7388c00138fc0d
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-04 11:38:34 +0200

    copiato esercizio di laravel

---
+++ app/Models/Project.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+use App\Models\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'slug',
+        'cover_image',
+        'type_id'
+    ];
+
+    // protected $table = "projects";
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
